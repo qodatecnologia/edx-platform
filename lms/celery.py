d177b5d0307aca30696b2c8004ce7c97f6d0661d
Awais Qureshi <awais.qureshi@arbisoft.com>
2020-11-17 19:13:46 +0500

    Adding logs.

--- lms/celery.py
+++ lms/celery.py
@@ -9,7 +9,6 @@ Taken from: https://celery.readthedocs.org/en/latest/django/first-steps-with-dja
 import os
 
 from celery import Celery
-from django.conf import settings
 
 from openedx.core.lib.celery.routers import AlternateEnvironmentRouter
 
@@ -22,7 +21,7 @@ APP.conf.task_protocol = 1
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
 APP.config_from_object('django.conf:settings')
-APP.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+APP.autodiscover_tasks()
 
 
 class Router(AlternateEnvironmentRouter):
